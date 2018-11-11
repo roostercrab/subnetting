cf5accbeb8e204509cb38c9d36a5ad0dd0c4f6c6
Jesse <jesse.shea@qixingdesign.com>
2018-11-11 14:03:44 -0800

    Fixed mask and ip sanity checks

--- ip_sanity_check.py
+++ ip_sanity_check.py
@@ -1,12 +1,12 @@
-#ip sanity check
-from ipaddress import ip_address
-
 def check_ip(ip):
 
     ip_array = [int(i) for i in ip.split('.')]
+    print(ip_array)
     for octet in ip_array:
         if octet > 255:
             print("Dotted decimal addresses can't be above 255 for any octet, please try again.")
             return False
         else:
-            return True
+            continue
+        print("This is a good IP address")
+        return True
