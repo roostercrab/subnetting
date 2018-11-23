e09b2a8f8910fa0afda2962b56650289eb86d714
Jesse <jesse.shea@qixingdesign.com>
2018-11-23 12:15:13 -0800

    Going to try adding code to window

--- window.py
+++ window.py
@@ -2,25 +2,27 @@ from tkinter import *
 
 root = Tk()
 
+the_label = Label(root, text="Please enter the IP address")
+the_label.pack()
+
+e = Entry(root)
+e.pack()
+
+e.delete(0, END)
+e.insert(0, "Please enter IP address")
+
+def removeValue(event):
+    self.entry.delete(0, 'end')
+    return None
+
 top_frame = Frame(root)
 top_frame.pack()
 
 bottom_frame = Frame(root)
 bottom_frame.pack(side = BOTTOM)
 
-button1 = Button(top_frame, text="Button 1", fg="red")
-button2 = Button(top_frame, text="Button 2", fg="blue")
-button3 = Button(top_frame, text="Button 3", fg="green")
-button4 = Button(bottom_frame, text="Button 4", fg="purple")
+button1 = Button(top_frame, text="Calculate Subnet", fg="red")
 
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack(side=BOTTOM)
+button1.pack(side=RIGHT)
 
 root.mainloop()
-
-
-
-#the_label = Label(root, text="This is text")
-#the_label.pack()
